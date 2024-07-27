fd895899b606af75f695c88550e7b93d67508c35
Watanabe Takashi <wtnb75@gmail.com>
2024-07-27 23:50:33 +0900

    setup

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from setuptools import setup
+from setuptools import setup, Extension
 
 extras_require = {
     x.stem.split("-", 1)[-1]: x.read_text().splitlines()
@@ -8,5 +8,9 @@ extras_require = {
 
 setup(
     install_requires=Path("requirements.txt").read_text().splitlines(),
+    setup_requires=["setuptools>=18.0", "cython"],
+    ext_modules=[
+        Extension("fmtparse.printf_bin", sources=["fmtparse/printf_bin.pyx"]),
+    ],
     extras_require=extras_require
 )
