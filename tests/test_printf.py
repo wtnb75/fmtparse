52b2982b33598b68188f0756d5e8d313783e44a6
Watanabe Takashi <wtnb75@gmail.com>
2024-07-28 00:15:35 +0900

    update

--- tests/test_printf.py
+++ tests/test_printf.py
@@ -1,59 +1,41 @@
 import unittest
-import glob
-import os
-import subprocess
-import importlib
+import fmtparse.printf
+import fmtparse.printf_bin
 
-simple_p = [
-    ("hello %s %d %(hello)s",
-     [(None, "hello ", None), ("s", '', ''), (None, " ", None), ("d", '', ''), (None, " ", None), ("s", '', 'hello')]),
-    ("%s %.2f %02x %>12d",
-     [("s", ''), (None, " "), ("f", '.2'), (None, " "), ("x", "02"), (None, " "), ("d", ">12")]),
+simple_glibc = [
+    ("%s %.2f %02x %12d",
+     [("s", '', ''), (None, " ", None), ("f", '.2', ''), (None, " ", None),
+      ("x", "02", ""), (None, " ", None), ("d", "12", "")]),
     ("%s %%s %%%s",
-     [("s", ''), (None, " "), (None, "%s "), (None, "%"), ("s", "")]),
-    ("%()s %(%)s %(%%)s %(s)d",
-     [("s", ''), (None, " "), ("s", "%"), (None, " "), ("s", "%%"), (None, " "), ("d", "s")]),
+     [('s', '', ''), (None, ' ', None), ('%', '', ''),
+      (None, 's ', None), ('%', '', ''), ('s', '', '')]),
 ]
 
-
-class TestCProstr(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
-        subprocess.call(["cythonize", "-i", "--3str",
-                        os.path.join(os.path.dirname(__file__), "..", "fmtparse", "printf_bin.pyx")],
-                        env=dict(CFLAGS="-DCYTHON_TRACE=1", **os.environ))
-        mod = importlib.import_module("fmtparse.printf_bin")
-        cls.parse_python = staticmethod(mod.parse_python)
-
-    @classmethod
-    def tearDownClass(cls):
-        if os.getenv("CYTHON_CLEANUP", None):
-            for k in glob.glob(os.path.join(os.path.dirname(__file__), "..", "fmtparse", "printf_bin.c*")):
-                os.unlink(k)
-
-    def setUp(self):
-        pass
-
-    def tearDown(self):
-        pass
-
-    def test_simple(self):
-        for a, b in simple_p:
-            self.assertEqual(b, list(self.parse_python(a)))
+simple_python = [
+    ("hello %s %d %(hello)s",
+     [(None, "hello ", None), ("s", '', ''), (None, " ", None),
+      ("d", '', ''), (None, " ", None), ("s", '', 'hello')]),
+    ("%()s %(%)s %(%%)s %(s)d",
+     [("s", '', ''), (None, " ", None), ("s", "", "%"), (None, " ", None),
+      ("s", "", "%%"), (None, " ", None), ("d", "", "s")]),
+]
 
 
 class TestProstr(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        mod = importlib.import_module("fmtparse.printf_py")
-        cls.parse_python = staticmethod(mod.parse_python)
+        cls.mod = fmtparse.printf
 
-    def setUp(self):
-        pass
+    def test_simple_python(self):
+        for a, b in simple_python:
+            self.assertEqual(b, list(self.mod.parse_python(a)))
 
-    def tearDown(self):
-        pass
+    def test_simple_glibc(self):
+        for a, b in simple_glibc:
+            self.assertEqual(b, list(self.mod.parse_glibc(a)))
 
-    def test_simple(self):
-        for a, b in simple_p:
-            self.assertEqual(b, list(self.parse_python(a)))
+
+class TestProstr_cython(TestProstr):
+    @classmethod
+    def setUpClass(cls):
+        cls.mod = fmtparse.printf_bin
