af8d498b2031b059d59eae86920470d969f674f7
Watanabe Takashi <wtnb75@gmail.com>
2024-08-05 20:24:46 +0900

    echo $var

---
+++ fmtparse/dollar.py
@@ -0,0 +1,100 @@
+import functools
+from enum import Enum, auto
+from collections.abc import Generator
+from typing import Optional
+from logging import getLogger
+from .wellknown import dollar_wellknown
+
+_log = getLogger(__name__)
+
+
+class Dstate(Enum):
+    normal = auto()
+    dollar = auto()
+    dollar2 = auto()
+    in_par = auto()
+    in_opt = auto()
+    bslash = auto()
+
+# mode: '${}:'
+
+
+def parse(s: str, mode: str, var_chars: str) -> Generator[tuple[str, str, Optional[str]], None, None]:
+    state = Dstate.normal
+    text_val = ""
+    dollar_val = ""
+    opt_val = ""
+    for c in s:
+        _log.debug("char: %s, state=%s", repr(c), state)
+        if state == Dstate.normal:
+            if c == mode[0]:
+                state = Dstate.dollar
+                dollar_val = ""
+                if text_val:
+                    _log.debug("yield text %s", repr(text_val))
+                    yield "text", text_val, None
+                    text_val = ""
+                continue
+            if c == '\\':
+                state = Dstate.bslash
+                continue
+            text_val += c
+        elif state == Dstate.dollar:
+            if c == mode[1]:
+                state = Dstate.in_par
+                continue
+            state = Dstate.dollar2
+            dollar_val += c
+        elif state == Dstate.dollar2:
+            if c not in var_chars:
+                _log.debug("yield var %s %s", repr(dollar_val), repr(opt_val))
+                yield "var", dollar_val, opt_val
+                state = Dstate.normal
+                dollar_val = ""
+                text_val = c
+                opt_val = ""
+                continue
+            dollar_val += c
+        elif state == Dstate.in_par:
+            if c == mode[2]:
+                _log.debug("yield var{} %s %s", repr(dollar_val), repr(opt_val))
+                yield "var", dollar_val, opt_val
+                dollar_val = ""
+                opt_val = ""
+                text_val = ""
+                state = Dstate.normal
+                continue
+            if len(mode) > 3 and c == mode[3]:
+                state = Dstate.in_opt
+                continue
+            dollar_val += c
+        elif state == Dstate.in_opt:
+            if c == mode[2]:
+                _log.debug("yield var{:} %s %s", repr(dollar_val), repr(opt_val))
+                yield "var", dollar_val, opt_val
+                dollar_val = ""
+                opt_val = ""
+                text_val = ""
+                state = Dstate.normal
+                continue
+            opt_val += c
+        elif state == Dstate.bslash:
+            text_val += c
+            state = Dstate.normal
+    if text_val:
+        _log.debug("yield-rest text %s", repr(text_val))
+        yield "text", text_val, None
+    if dollar_val:
+        _log.debug("yield-rest var %s %s", repr(dollar_val), repr(opt_val))
+        yield "var", dollar_val, opt_val
+    _log.debug("finished")
+
+
+def parse_dr(s: str, mode: str) -> Generator[tuple[Optional[str], str, str], None, None]:
+    yield from parse(s, *dollar_wellknown[mode])
+
+
+for k in dollar_wellknown.keys():
+    def _(s):
+        return parse_dr(s, k)
+    locals()[f"parse_{k}"] = functools.partial(parse_dr, mode=k)
